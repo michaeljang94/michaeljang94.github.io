53dd73da2c88be39678dd693df059f1c5a2113fe
Michael <michaeljang94@gmail.com>
2025-12-31 17:09:32 +1300

    Create vite project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { SideNavigationBar } from './components/SideNavigationBar';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { AppBar, Button, Drawer, List, Toolbar, Typography } from '@mui/material'
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <SideNavigationBar></SideNavigationBar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+    <>
+    <AppBar position="static">
+      <Toolbar>
+<IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+    </AppBar>
+    </>
+  )
+}
+
+export default App
