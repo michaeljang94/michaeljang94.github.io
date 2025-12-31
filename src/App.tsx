819ad6267b7ac6c68cfd3faddede677cbe865735
Michael <michaeljang94@gmail.com>
2025-12-31 17:10:57 +1300

    Removed module

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { AppBar, Button, Drawer, List, Toolbar, Typography } from '@mui/material'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { AppBar, Button, Drawer, List, Toolbar, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <AppBar position="static">
-      <Toolbar>
-<IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -27,9 +21,9 @@ function App() {
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
-    </AppBar>
+      </AppBar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
